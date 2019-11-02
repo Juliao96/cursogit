56774ba0ea45abf28e0da2b06b9757b031f114ed
Juliao Gomes Gime <juliaogimi@gmail.com>
2019-11-02 16:37:07 +0300

    Foi adcionado a segunda linha

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace TreinandoOGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
             Console.WriteLine("Como voce esta hoje?");
             Console.WriteLine("Tenha um bom dia");
             Console.ReadLine();
